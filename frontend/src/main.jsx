812fb619eec09d6d86c99ca3cb27730224338dd6
Fatma <fatma.abdullayewa@gmail.com>
2026-07-19 20:24:06 +0300

    Add useAuthContext and useWorkoutsContext hooks

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthContextProvider } from "./context/AuthContext.js";
+import { WorkoutsContextProvider } from "./context/WorkoutContext.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthContextProvider>
+      <WorkoutsContextProvider>
+        <App />
+      </WorkoutsContextProvider>
+    </AuthContextProvider>
   </StrictMode>,
-)
+);
