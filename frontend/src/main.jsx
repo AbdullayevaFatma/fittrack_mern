0355806399edf9b3957e89aa1f6019bedc71839c
Fatma <fatma.abdullayewa@gmail.com>
2026-07-19 21:48:43 +0300

    server.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { AuthContextProvider } from "./context/AuthContext.js";
-import { WorkoutsContextProvider } from "./context/WorkoutContext.js";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { WorkoutsContextProvider } from "./context/WorkoutContext.jsx";
+
+
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
